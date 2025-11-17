764848f94449faac7cf960a92bac4c2c96748fe7
U1_Projecyt <kimberlyz26@nycstudents.net>
2025-11-16 20:47:40 -0500

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,47 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("BEEP! BEEP! BEEP!");
-        System.out.println("Do you wake up or press snooze? (<= 3 wake up, > 3 press snooze)");
-        System.out.println(RollDie.rollDie());
-        if (RollDie.rollDie() <= 3){
-            System.out.println("Uh oh! you woke up late and missed the train! :(");
-            RollDie.rollDie();
-            if (RollDie.rollDie() <= 3){
+        System.out.println("Do you wake up or press snooze?");
+        int num = RollDie.rollDie();
+        if (num <= 3){
+            System.out.println("Uh oh! You woke up late and missed the train! :(");
+            System.out.println("However there's still hope!");
+            num = RollDie.rollDie();
+            if (num <= 2){
                 System.out.println("Lucky you! There's another train. You make it to school just in time!");
             } else {
-                System.out.println("The train is delayed :(. You waited for 20 minutes and it's still not here." +
-                        "You get to school late.");
+                System.out.println("The train is delayed :( You waited for 20 minutes and it's still not here." +
+                        " You get to school late.");
             }
         } else {
             System.out.println("You made it to school with extra time to spare!");
+        }
+
+        System.out.println("Regardless of when you made it to school, you have a test today!");
+        System.out.println("Did you study?");
+        String answer = scan.nextLine();
+        answer = answer.toLowerCase();
+
+        if (answer.equals("yes")){
+            System.out.println("Lets see how you did!");
+            num = RollDie.rollDie();
+            if (num <= 3){
+                System.out.println("Woohoo! You passed with flying colors!");
+            } else {
+                System.out.println("It's okay. You'll do better nexT time :)");
+            }
+        } else {
+            System.out.println("Lets see how you did!");
+            num = RollDie.rollDie();
+            if (num <= 3){
+                System.out.println("Woah! great job!");
+            } else {
+                System.out.println("Maybe you should study next time.");
+            }
         }
 
         scan.close();
--- src/RollDie.java
+++ src/RollDie.java
@@ -1,6 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class RollDie{
     public static int rollDie(){
+        Scanner scan = new Scanner(System.in);
         int num = (int) ((Math.random() * 6) + 1);
-        return num;
+        System.out.println("Do you want to continue?");
+        String response = scan.nextLine();
+        response = response.toLowerCase();
+
+        if (response.equals("yes")){
+            System.out.println("Result: " + num);
+            return num;
+        } else {
+            System.out.println("Too bad");
+            System.out.println("Result: " + num);
+            return num;
+        }
     }
 }
